18dfade92f6af25560e8c3d061efb120848f671d
ErfanMhi <mhi.erfan1@gmail.com>
2024-12-26 22:48:55 -0500

    fix: resolve IP address configuration in Terraform setup
    - Fixed incorrect IP address assignments in worker node configurations
    - Updated network interface configurations for master-worker communication
    
    Note: Changes pending testing in staging environment

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,6 @@
 locals {
   fs_device_name = "training-vm-filesystem-1"
+  # master_ip = try(nebius_compute_v1_instance.training_h100_master.network_interfaces[0].ip_address.address, "")
 }
 
 data "nebius_vpc_v1_subnet" "default" {
@@ -32,9 +33,9 @@ resource "nebius_compute_v1_gpu_cluster" "training_fabric_3" {
   infiniband_fabric = "fabric-3"
 }
 
-resource "nebius_compute_v1_instance" "training_h100" {
-  count = var.cluster_size
-  name  = "training-h100-${count.index + 1}"
+# Master instance
+resource "nebius_compute_v1_instance" "training_h100_master" {
+  name  = "training-h100-master"
   
   parent_id = var.project_id
   gpu_cluster = {
@@ -74,8 +75,59 @@ resource "nebius_compute_v1_instance" "training_h100" {
     epochs             = var.training_epochs
     save_frequency     = var.save_frequency
     batch_size         = var.training_batch_size
-    instance_index     = count.index
+    instance_index     = 0
     cluster_size       = var.cluster_size
-    master_ip          = count.index == 0 ? "" : nebius_compute_v1_instance.training_h100[0].network_interfaces[0].ip_address
+    master_ip          = ""
+  })
+}
+
+# Worker instances
+resource "nebius_compute_v1_instance" "training_h100_workers" {
+  count = var.cluster_size - 1
+  name  = "training-h100-${count.index}"
+  
+  depends_on = [nebius_compute_v1_instance.training_h100_master]
+  
+  parent_id = var.project_id
+  gpu_cluster = {
+    id = nebius_compute_v1_gpu_cluster.training_fabric_3.id
+  }
+  network_interfaces = [
+    {
+      name              = "eth0"
+      subnet_id         = data.nebius_vpc_v1_subnet.default.id
+      ip_address        = {}
+      public_ip_address = {}
+    }
+  ]
+  resources = {
+    platform = "gpu-h100-sxm"
+    preset   = "8gpu-128vcpu-1600gb"
+  }
+  boot_disk = {
+    attach_mode   = "READ_WRITE"
+    existing_disk = {
+      id = nebius_compute_v1_disk.training_boot_200gib.id
+    }
+  }
+  filesystems = [
+    {
+      attach_mode         = "READ_WRITE",
+      mount_tag           = local.fs_device_name
+      existing_filesystem = {
+        id = nebius_compute_v1_filesystem.training_1024gib.id
+      }
+    }
+  ]
+  cloud_init_user_data = templatefile("${path.module}/scripts/cloud-init.tftpl", {
+    vm_username         = var.vm_username
+    ssh_public_key      = file(var.vm_ssh_public_key_path)
+    fs_device_name      = local.fs_device_name
+    epochs             = var.training_epochs
+    save_frequency     = var.save_frequency
+    batch_size         = var.training_batch_size
+    instance_index     = count.index + 1
+    cluster_size       = var.cluster_size
+    master_ip          = nebius_compute_v1_instance.training_h100_master.status.network_interfaces[0].ip_address.address
   })
 }
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,4 +1,11 @@
-output "training_public_ip" {
-  description = "The public IP address of the training VM"
-  value = trimsuffix(nebius_compute_v1_instance.training_h100.status.network_interfaces[0].public_ip_address.address, "/32")
+output "master_public_ip" {
+  description = "The public IP address of the master training VM"
+  value = trimsuffix(nebius_compute_v1_instance.training_h100_master.status.network_interfaces[0].public_ip_address.address, "/32")
+}
+
+output "worker_public_ips" {
+  description = "The public IP addresses of the worker training VMs"
+  value = [for instance in nebius_compute_v1_instance.training_h100_workers : 
+    trimsuffix(instance.status.network_interfaces[0].public_ip_address.address, "/32")
+  ]
 }
